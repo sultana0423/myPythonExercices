67c5488b08d2f07a2e58f03f00b9a2786c8d67e4
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-09-16 16:17:15 +0300

    Update TempCover.py

--- TempCover.py
+++ TempCover.py
@@ -13,15 +13,29 @@ userChoice = int(input("Choose an operation: "))
 if userChoice == 1: 
     degree = int(input("Degree in Fahrenheit: "))
     toCelsius = round((degree - 32) * 5 / 9, 2)
-    print(f"{degree} F to C is {toCelsius}C")
+    print(f"{degree}F to C is {toCelsius}C")
 elif userChoice == 2: 
     degree = int(input("Degree in Celsius: "))
     toFahren = round((degree * 9 / 5) + 32,2)
-    print(f"{degree} C to F is {toFahren}F")
+    print(f"{degree}C to F is {toFahren}F")
 
 elif userChoice == 3:
     degree = int(input("Degree in Fahrenheit: "))
     toKelv = round(((degree - 32) / 1.8) + 273.15 , 2)
-    print(f"{degree} F to K is {toKelv}K")
+    print(f"{degree}F to K is {toKelv}K")
 elif userChoice == 4:
+    degree = int(input("Degree in Kelvin: "))
+    toFahren = round(((degree - 273.15) * 1.8) + 32 , 2)
+    print(f"{degree}K to F is {toFahren}F")
+elif userChoice == 5:
+    degree = int(input("Degree in Celsius: "))
+    toKelv = round(degree + 273.15 , 2)
+    print(f"{degree}C to K is {toKelv}K")
+elif userChoice == 6:
+    degree = int(input("Degree in Kelvin: "))
+    toCelsius = round(degree - 273.15 , 2)
+    print(f"{degree}K to C is {toCelsius}C")
+else:
+    print("Please choose from the above operations")
+    
     
