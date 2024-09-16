4b73c27d50d3d02fe7ddb6bc9ef5f2d32f3139cb
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-09-16 12:50:45 +0300

    Create TempCover.py

---
+++ TempCover.py
@@ -0,0 +1,27 @@
+# Create a program that converts temperatures between Celsius, Fahrenheit, and Kelvin.
+
+print("1- Fahrenheit to Celsius")
+print("2- Celsius to Fahrenheit")
+print("3- Fahrenheit to Kelvin")
+print("4- Kelvin to Fahrenheit")
+print("5- Celsius to Kelvin")
+print("6- Kelvin to Celsius")
+
+
+userChoice = int(input("Choose an operation: "))
+
+if userChoice == 1: 
+    degree = int(input("Degree in Fahrenheit: "))
+    toCelsius = round((degree - 32) * 5 / 9, 2)
+    print(f"{degree} F to C is {toCelsius}C")
+elif userChoice == 2: 
+    degree = int(input("Degree in Celsius: "))
+    toFahren = round((degree * 9 / 5) + 32,2)
+    print(f"{degree} C to F is {toFahren}F")
+
+elif userChoice == 3:
+    degree = int(input("Degree in Fahrenheit: "))
+    toKelv = round(((degree - 32) / 1.8) + 273.15 , 2)
+    print(f"{degree} F to K is {toKelv}K")
+elif userChoice == 4:
+    
