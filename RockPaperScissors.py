764cec3c34710d6491bf9088248bf9d985902cc6
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-10-09 19:42:40 +0300

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,1 +1,18 @@
 # Rock, Paper, Scissors Game: Implement the classic rock, paper, scissors game where the user plays against the computer.
+# Rock, Paper, Scissors Game: Implement the classic rock, paper, scissors game where the user plays against the computer.
+import random
+print("Rock, Paper, Scissors Game")
+print("1- Rock")
+print("2- Paper")
+print("3- Scissors")
+randomPick = random.randrange(1,3)
+userI = int(input("Your pick: "))
+
+rock = 1
+paper = 2
+scissor = 3
+
+if userI == rock:
+    print("Computer: Rock")
+    print(f"You: {}")
+    print("Try again")
