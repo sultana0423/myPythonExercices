ee8ea46c81f9f549b88e4ce86bd9475bb917c8bc
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-10-20 21:06:24 +0300

    Update intrestCalc.py

--- intrestCalc.py
+++ intrestCalc.py
@@ -1,6 +1,9 @@
 #Simple Interest Calculator: Create a program that calculates simple interest based on the input of principal, rate of interest, and time.
 print("Simple Interest Calculator")
-principal = int(input("Principal: "))
-rateOfInterest = int(input("Rate of Interest: "))
-time = int(input("Time: ")) 
+p = int(input("Principal: "))
+r = int(input("Rate of Interest: ")) / 100
+t = int(input("Time: ")) 
 def intrestCal(p,r,t):
+  return p*r*t
+
+print(intrestCal(p,r,t))
